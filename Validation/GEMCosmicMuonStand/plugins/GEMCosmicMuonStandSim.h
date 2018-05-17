a756bc6f51006356415184a784b94cd9abdbcc18
slowmoyang <slowmoyang@gmail.com>
2018-05-17 20:00:46 +0900

    1. GEMCosmicStandSim Add monitor elements for residual and pull per VFAT
    2. gemDrawSim
    Change the structure of a plotting script.
    One output histogram is created by one Painter instance.
    There are two types of Painter class:
        HistPainter - Painter for the histogram
        EffPainter for the efficiency
    And both of them inherit from the BasePainter.

--- Validation/GEMCosmicMuonStand/plugins/GEMCosmicMuonStandSim.h
+++ Validation/GEMCosmicMuonStand/plugins/GEMCosmicMuonStandSim.h
@@ -28,53 +28,75 @@ class GEMCosmicMuonStandSim : public DQMEDAnalyzer {
                              TString title,
                              Int_t nchX, Double_t lowX, Double_t highX);
 
+
+  MonitorElement* BookHist1D(DQMStore::IBooker &ibooker,
+                             TString title,
+                             Int_t lowX, Int_t highX);
+
   MonitorElement* BookHist2D(DQMStore::IBooker &ibooker,
                              TString title,
                              Int_t nchX, Double_t lowX, Double_t highX,
                              Int_t nchY, Double_t lowY, Double_t highY);
 
-
+  MonitorElement* BookHist2D(DQMStore::IBooker &ibooker,
+                             TString title,
+                             Int_t lowX, Int_t highX,
+                             Int_t lowY, Int_t highY);
 
   // Utility functions
-
   Int_t GetVFATId(Float_t x, const GEMEtaPartition* roll);
-
   Int_t GetChamberIndex(Int_t chamber_id) {return (chamber_id - 1) / 2; }
-
   Int_t GetOverallVFATPlotY(Int_t roll_id, Int_t vfat_id) {return 3 * roll_id + vfat_id - 3;}
-
   // conversion_factor = 10 deg * ( TMath::Pi() / 180 deg ) / 384 = 0.00045451283
   Float_t GetLocalPhi(Float_t strip) {return 0.00045451283 * (strip - 192);}
-
-
-
 
  private:
   edm::EDGetTokenT<edm::PSimHitContainer> sim_hit_token_;
   edm::EDGetTokenT<GEMRecHitCollection> rec_hit_token_;
 
-  const Int_t kNumChamberId_ = 15, kMinChamberId_ = 1, kMaxChamberId_ = 29;
-  const Int_t kNumRollId_ = 8, kMinRollId_ = 1, kMaxRollId_ = 8;
-  const Int_t kNumVFATId_ = 3, kMinVFATId_ = 1, kMaxVFATId_ = 3;
-  const Int_t kNumStripId_ = 385, kMinStripId_ = 0, kMaxStripId_ = 384;
+  // Global constants
+  const Int_t kMinChamberId_ = 1, kMaxChamberId_ = 29;
+  const Int_t kMinRollId_ = 1, kMaxRollId_ = 8;
+  const Int_t kMinVFATId_ = 1, kMaxVFATId_ = 3;
 
-  MonitorElement *me_vfat_passed_, *me_vfat_total_;
-  MonitorElement *me_vfat_passed_per_chamber_[15];
-  MonitorElement *me_vfat_total_per_chamber_[15];
+  const Int_t kNumRow_ = 5, kNumColumn_ = 3; // # of rows and columns in the stand
+  const Int_t kNumSuperchamber_ = 15; // # of superchambers in the stand
+  const Int_t kNumLayer_ = 2; // # of layers (chambers) in each superchamber
+  const Int_t kNumRoll_ = 8; // # of roll (eta partition) in each chamber
+  const Int_t kNumVFAT_ = 3; // # of VFATs in each roll.
+  const Int_t kNumStrip_ = 384; // # of strips in each roll.
 
-  MonitorElement *me_vfat_occupancy_;
-  MonitorElement *me_vfat_occupancy_per_chamber_[15];
+  const Int_t k1Deg_ = TMath::Pi() / 180.0;
 
-  MonitorElement *me_residual_local_x_, *me_residual_local_y_, *me_residual_local_phi_;
-  MonitorElement *me_error_x_, *me_error_y_;
-  MonitorElement *me_pull_local_x_, *me_pull_local_y_;
 
+  // Monitor Element
+
+  // passed and total for efficiency
+  MonitorElement *me_vfat_passed_overall_, *me_vfat_total_overall_;
+  MonitorElement *me_vfat_passed_[15][2]; // [superchamber][layer]
+  MonitorElement *me_vfat_total_[15][2];
+
+  // occupancy
+  MonitorElement *me_occ_overall_, *me_occ_overall_v2_;
+  MonitorElement *me_vfat_occ_[15][2];
+
+  // residual, error, pull
+  MonitorElement *me_res_x_overall_, *me_res_y_overall_, *me_res_phi_overall_;
+  MonitorElement *me_error_x_overall_, *me_error_y_overall_;
+  MonitorElement *me_pull_x_overall_, *me_pull_y_overall_;
+
+  MonitorElement *me_res_x_[15][2][8][3];
+  MonitorElement *me_res_y_[15][2][8][3];
+  MonitorElement *me_res_phi_[15][2][8][3];
+  MonitorElement *me_pull_x_[15][2][8][3];
+  MonitorElement *me_pull_y_[15][2][8][3];
+
+  // extra
   MonitorElement *me_cls_, *me_cls_vs_chamber_;
-  MonitorElement *me_num_clusters_vs_chamber_, *me_num_clusters_;
+  MonitorElement *me_num_clusters_, *me_num_clusters_vs_chamber_;
   MonitorElement *me_num_sim_hits_, *me_num_rec_hits_;
-  MonitorElement *me_sim_hit_bare_local_phi_, *me_sim_hit_local_phi_, *me_rec_hit_local_phi_;
+  MonitorElement *me_sim_phi_, *me_rec_phi_;
   MonitorElement *me_sim_rec_distance_;
-  MonitorElement *me_mat_chamber_, *me_mis_chamber_;
   MonitorElement *me_sim_strip_, *me_rec_first_strip_; //, *me_strip_diff_, ;
 };
 
