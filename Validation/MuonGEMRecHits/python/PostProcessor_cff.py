1647f539448116411a5995aebc5069ddfc7655e3
slowmoyang <slowmoyang@gmail.com>
2018-08-01 02:25:09 +0900

    Change the detector ids fro 2023 geometry

--- Validation/MuonGEMRecHits/python/PostProcessor_cff.py
+++ Validation/MuonGEMRecHits/python/PostProcessor_cff.py
@@ -2,8 +2,8 @@ import FWCore.ParameterSet.Config as cms
 from DQMServices.Core.DQMEDHarvester import DQMEDHarvester
 
 gemRecHitHarvesting = DQMEDHarvester("MuonGEMRecHitsHarvestor",
-    regionIds = cms.untracked.vint32(-1),                                           
-    stationIds = cms.untracked.vint32(1),                                           
+    regionIds = cms.untracked.vint32(-1, 1),                                           
+    stationIds = cms.untracked.vint32(1, 2), 
     layerIds = cms.untracked.vint32(1, 2),
 )
 MuonGEMRecHitsPostProcessors = cms.Sequence(gemRecHitHarvesting) 
