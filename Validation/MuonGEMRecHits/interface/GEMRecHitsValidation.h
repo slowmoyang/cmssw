2b5fcab5780971c403234759f2c3f9c182bc1b1e
slowmoyang <slowmoyang@gmail.com>
2018-11-21 22:44:21 +0900

    Add occupancy monitor elements for efficiencies. The definition of efficiency changes as follows. [OLD] (matched rechit occupancy) / (simhit occupancy) [NEW] (matched rechit occupancy) / (muon occupancy)

--- Validation/MuonGEMRecHits/interface/GEMRecHitsValidation.h
+++ Validation/MuonGEMRecHits/interface/GEMRecHitsValidation.h
@@ -1,42 +1,91 @@
-#ifndef GEMRecHitsValidation_H
-#define GEMRecHitsValidation_H
+#ifndef VALIDATION_MUONGEMRECHITS_INTERFACE_GEMRECHITSVALIDATION_H_
+#define VALIDATION_MUONGEMRECHITS_INTERFACE_GEMRECHITSVALIDATION_H_
 
 #include "Validation/MuonGEMHits/interface/GEMBaseValidation.h"
+#include "Validation/MuonGEMHits/interface/GEMValidationUtils.h"
 
-#include <DataFormats/GEMRecHit/interface/GEMRecHit.h>
-#include <DataFormats/GEMRecHit/interface/GEMRecHitCollection.h>
+#include "DataFormats/GEMRecHit/interface/GEMRecHit.h"
+#include "DataFormats/GEMRecHit/interface/GEMRecHitCollection.h"
+#include "DataFormats/PatCandidates/interface/Muon.h"
+#include "DataFormats/MuonReco/interface/MuonFwd.h"
+#include "TrackingTools/TransientTrack/interface/TransientTrackBuilder.h"
+#include "TrackingTools/Records/interface/TransientTrackRecord.h"
+#include "RecoMuon/TrackingTools/interface/MuonServiceProxy.h"
 
-class GEMRecHitsValidation : public GEMBaseValidation
-{
-public:
-  explicit GEMRecHitsValidation( const edm::ParameterSet& );
+
+class GEMRecHitsValidation : public GEMBaseValidation {
+ public:
+  explicit GEMRecHitsValidation(const edm::ParameterSet &);
   ~GEMRecHitsValidation() override;
-  void bookHistograms(DQMStore::IBooker &, edm::Run const &, edm::EventSetup const &) override;
-  void analyze(const edm::Event& e, const edm::EventSetup&) override;
-  MonitorElement* BookHist1D( DQMStore::IBooker &, const char* name, const char* label, unsigned int region_num, unsigned int station_num, unsigned int layer_num, const unsigned int Nbin, const Float_t xMin, const Float_t xMax);
-  MonitorElement* BookHist1D( DQMStore::IBooker &, const char* name, const char* label, unsigned int region_num, const unsigned int Nbin, const Float_t xMin, const Float_t xMax);
+  void bookHistograms(DQMStore::IBooker &,
+                      edm::Run const &,
+                      edm::EventSetup const &) override;
+  void analyze(const edm::Event &, const edm::EventSetup &) override;
 
-private:
-
-  //Detaile Plots
-  MonitorElement* gem_rh_xy[2][3][2];
-  MonitorElement* gem_rh_zr[2][3][2];
-  MonitorElement* gem_cls[2][3][2];
-  MonitorElement* gem_pullX[2][3][2];
-  MonitorElement* gem_pullY[2][3][2];
-
+ private:
+  const GEMEtaPartition * findEtaPartition(const GEMChamber* chamber,
+                                           GlobalPoint & global_point);
 
   //Simple Plots
-  MonitorElement* gem_cls_tot;
-  std::unordered_map< UInt_t , MonitorElement* > recHits_dcEta;
-  std::unordered_map< UInt_t , MonitorElement* > recHits_simple_zr;
-  MonitorElement* gem_region_pullX[2];
-  MonitorElement* gem_region_pullY[2];
+  MonitorElement* me_cls_;
 
-  edm::EDGetToken InputTagToken_, InputTagToken_RH;
-  int nBinXY_;
-  bool detailPlot_;
+  MEMap1Ids me_residual_x_;
+  MEMap1Ids me_residual_y_;
+  MEMap1Ids me_pull_x_;
+  MEMap1Ids me_pull_y_;
+
+  MEMap2Ids me_residual_x_roll_;
+  MEMap2Ids me_residual_y_roll_;
+  MEMap2Ids me_pull_x_roll_;
+  MEMap2Ids me_pull_y_roll_;
+  MEMap2Ids me_cls_roll_;
+
+  MEMap1Ids me_rechit_occ_zr_;
+
+  // NOTE Efficiency = matched rechit occupancy / muon occupancy
+  // muon
+  MEMap1Ids me_muon_occ_eta_;
+  MEMap2Ids me_muon_occ_phi_;
+  MEMap2Ids me_muon_occ_det_;
+  // RecHit
+  MEMap1Ids me_rechit_occ_eta_;
+  MEMap2Ids me_rechit_occ_phi_;
+  MEMap2Ids me_rechit_occ_det_;
+
+  // Detaile Plots
+
+  // occupancy plots for efficiency
+  // first: tight <--> second: loose
+  std::pair<MEMap1Ids, MEMap1Ids> me_detail_muon_occ_eta_;
+  std::pair<MEMap2Ids, MEMap2Ids> me_detail_muon_occ_phi_;
+  std::pair<MEMap2Ids, MEMap2Ids> me_detail_muon_occ_det_;
+  std::pair<MEMap1Ids, MEMap1Ids> me_detail_rechit_occ_eta_;
+  std::pair<MEMap2Ids, MEMap2Ids> me_detail_rechit_occ_phi_;
+  std::pair<MEMap2Ids, MEMap2Ids> me_detail_rechit_occ_det_;
+
+  // just occupancy plots
+  MEMap3Ids me_detail_rechit_occ_xy_;
+  MEMap3Ids me_detail_rechit_occ_xy_ch1_;
+  MEMap3Ids me_detail_rechit_occ_zr_;
+  MEMap3Ids me_detail_rechit_occ_polar_;
+
+  // 
+  MEMap3Ids me_detail_cls_;
+  MEMap3Ids me_detail_residual_x_;
+  MEMap3Ids me_detail_residual_y_;
+  MEMap3Ids me_detail_pull_x_;
+  MEMap3Ids me_detail_pull_y_;
+
+  //  
+  edm::EDGetToken rechit_token_;
+  edm::EDGetTokenT<edm::View<reco::Muon> > muon_token_;
+  MuonServiceProxy* muon_service_proxy_; 
+  Float_t muon_pt_cut_;
+
+  // constants
+  const std::string kPropagatorName_ = "SteppingHelixPropagatorAny";
+  const Int_t kLooseId_ = 1, kTightId_ = 2;
 
 };
 
-#endif
+#endif // VALIDATION_MUONGEMRECHITS_INTERFACE_GEMRECHITSVALIDATION_H_
